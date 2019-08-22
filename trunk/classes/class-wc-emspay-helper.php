3a2b744a0a719fbabbaca0edfcb9c37f02c13905
Maksim <maxim.volgust@leasoft.org>
2019-08-22 10:36:03 +0200

    Added check for empty value in getOrderLines function

--- trunk/classes/class-wc-emspay-helper.php
+++ trunk/classes/class-wc-emspay-helper.php
@@ -300,7 +300,7 @@ class WC_Emspay_Helper
 
         foreach ($order->get_items() as $orderLine) {
             $productId = (int) $orderLine->get_variation_id() ?: $orderLine->get_product_id();
-            $orderLines[] = [
+            $orderLines[] = array_filter([
                 'url' => get_permalink($productId),
                 'name' => $orderLine->get_name(),
                 'type' => \GingerPayments\Payment\Order\OrderLine\Type::PHYSICAL,
@@ -310,7 +310,10 @@ class WC_Emspay_Helper
                 'image_url' => wp_get_attachment_url($orderLine->get_product()->get_image_id()),
                 'vat_percentage' => static::getAmountInCents(static::getProductTaxRate($orderLine->get_product())),
                 'merchant_order_line_id' => $productId
-            ];
+            ],
+            function($value) {
+	            return ! is_null($value);
+	        });
         }
 
         if ($order->get_total_shipping() > 0) {
