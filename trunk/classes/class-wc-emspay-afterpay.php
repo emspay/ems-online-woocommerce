7efce717b33c7d8fad1e856806f92464c304f323
Maksim <maxim.volgust@leasoft.org>
2019-12-27 16:51:23 +0100

    minor fix

--- trunk/classes/class-wc-emspay-afterpay.php
+++ trunk/classes/class-wc-emspay-afterpay.php
@@ -42,7 +42,7 @@ class WC_Emspay_AfterPay extends WC_Emspay_Gateway
             'merchant_order_id' => (string) $order_id,
             'description' => WC_Emspay_Helper::getOrderDescription($order_id),
             'return_url' => WC_Emspay_Helper::getReturnUrl(),
-            'customer' => (WC_Emspay_Helper::getCustomerInfo($order),
+            'customer' => WC_Emspay_Helper::getCustomerInfo($order),
 			'extra' => ['plugin' => EMSPAY_PLUGIN_VERSION],
             'webhook_url' => WC_Emspay_Helper::getWebhookUrl($this),
             'order_lines' => WC_Emspay_Helper::getOrderLines($order)
