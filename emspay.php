b2d30a3c5fc33007a0cdc26b9a113e83071b5fe8
Maxim <maxim.volgust@leasoft.org>
2020-12-03 12:22:05 +0200

    version & changelog update

--- emspay.php
+++ emspay.php
@@ -4,7 +4,7 @@
  * Plugin Name: EMS Online
  * Plugin URI: https://emspay.nl/
  * Description: EMS Pay WooCommerce plugin
- * Version: 1.1.1
+ * Version: 1.1.2
  * Author: Ginger Payments
  * Author URI: https://www.gingerpayments.com/
  * License: The MIT License (MIT)
